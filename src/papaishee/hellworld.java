6116ec0e54493826b1642a59de56e5cb4bfa77a7
papaicts <papaishee8@gamil.com>
2022-06-01 01:39:22 +0530

    first commit

---
+++ src/papaishee/hellworld.java
@@ -0,0 +1,12 @@
+package papaishee;
+
+public class hellworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i am papai shee");
+		System.out.println("okok");
+
+	}
+
+}
